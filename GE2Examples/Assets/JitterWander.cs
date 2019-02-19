edcda0f3840a52687c4cb6da1d6ef3d5c363fda8
skooter500 <skooter500@gmail.com>
2019-02-19 16:22:40 +0000

    wander starter code

---
+++ GE2Examples/Assets/JitterWander.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JitterWander : SteeringBehaviour
+{
+    public float distance = 15.0f;
+    public float radius = 10;
+    public float jitter = 100;
+
+    Vector3 localTarget;
+    Vector3 worldTarget;
+
+    public override Vector3 Calculate()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
