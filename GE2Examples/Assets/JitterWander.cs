38897998779eca46208266b26f0e4317a2eb0273
skooter500 <skooter500@gmail.com>
2019-02-19 19:52:22 +0000

    Added Jitter Wander code

--- GE2Examples/Assets/JitterWander.cs
+++ GE2Examples/Assets/JitterWander.cs
@@ -8,18 +8,48 @@ public class JitterWander : SteeringBehaviour
     public float radius = 10;
     public float jitter = 100;
 
-    Vector3 localTarget;
+    Vector3 target;
     Vector3 worldTarget;
+
+    public void OnDrawGizmos()
+    {
+        Vector3 localCP = Vector3.forward * distance;
+        Vector3 worldCP = transform.TransformPoint(localCP);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(transform.position, worldCP);
+        Gizmos.DrawWireSphere(worldCP, radius);
+
+        Vector3 localTarget = (Vector3.forward * distance) + target;
+        worldTarget = transform.TransformPoint(localTarget);
+
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawSphere(worldTarget, 1);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(transform.position, worldTarget);
+
+    }
 
     public override Vector3 Calculate()
     {
-        
+        Vector3 disp = jitter * Random.insideUnitSphere * Time.deltaTime;
+        target += disp;
+        target.Normalize();
+        target *= radius;
+
+        Vector3 localTarget = (Vector3.forward * distance) + target;
+
+        worldTarget = transform.TransformPoint(localTarget);
+        return worldTarget - transform.position;
+
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = Random.insideUnitSphere * radius;
     }
 
     // Update is called once per frame
