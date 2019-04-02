d8e4f08d90b407331d50a4cc80e009eafa6cecd2
skooter500 <skooter500@gmail.com>
2019-04-02 16:55:28 +0100

    simple ECS example

---
+++ GE2Examples/Assets/Bootstrap.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+using Unity.Transforms;
+using Unity.Rendering;
+using Unity.Mathematics;
+using Unity.Jobs;
+
+public struct Sway : IComponentData
+{
+    public float theta;
+}
+
+public class Bootstrap : MonoBehaviour
+{
+    private EntityArchetype swayArchitype;
+
+    private RenderMesh swayMesh;
+    public Mesh mesh;
+    public Material material;
+
+    private EntityManager entityManager;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        entityManager = World.Active.GetOrCreateManager<EntityManager>();
+
+        swayArchitype = entityManager.CreateArchetype(typeof(Position), typeof(Rotation), typeof(Sway));
+
+        swayMesh = new RenderMesh();
+        swayMesh.mesh = mesh;
+        swayMesh.material = material;
+
+        for (int row = -100; row < 100; row++)
+        {
+            for (int col = -100; col < 100; col++)
+            {
+                Entity swayEntity = entityManager.CreateEntity(swayArchitype);
+
+                Position p = new Position();
+                p.Value = new float3(col * 2, 0, row * 2);
+
+                Rotation r = new Rotation();
+                r.Value = new quaternion();
+
+                entityManager.SetComponentData(swayEntity, p);
+                entityManager.SetComponentData(swayEntity, r);
+
+                entityManager.AddSharedComponentData(swayEntity, swayMesh);
+                
+            }
+        }
+    }
+}
+
+public struct SwayJob : IJobProcessComponentData<Position, Rotation, Sway>
+{
+    public float frequency;
+    public float amplitude;
+    public float timeDelta;
+    public void Execute(ref Position p, ref Rotation r, ref Sway s)
+    {
+        r.Value = Quaternion.AngleAxis(Mathf.Sin(s.theta) * amplitude, Vector3.right);
+        s.theta += Mathf.PI * 2.0f * frequency * timeDelta;
+    }
+}
+
+public class SwaySystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var sj = new SwayJob()
+        {
+            frequency = 0.5f
+            , amplitude = 60
+            , timeDelta = Time.deltaTime
+        };
+
+        return sj.Schedule(this, inputDeps);
+    }
+}
---
+++ GE2Examples/Assets/CubeSystem1.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using Unity.Entities;
+
+
+public class CubeSystem1 : ComponentSystem
+{
+    private struct Filter
+    {
+        public Transform t;
+        public CubeComponent cc;
+    }
+
+    protected override void OnUpdate()
+    {
+        float t = Time.deltaTime;
+        foreach (var cube in GetEntities<Filter>())
+        {
+            cube.t.transform.Translate(0, t, 0);
+            cube.t.localScale = new Vector3(1, Mathf.Sin(cube.cc.theta) * 5, 1);
+        }
+    }
+}
+
+public class CubeSystem2 : ComponentSystem
+{
+    private struct Data
+    {
+        public readonly int Length;
+        public ComponentArray<CubeComponent> components;
+    }
+
+    [Inject] private Data data;
+
+    protected override void OnUpdate()
+    {
+        float t = Time.deltaTime;
+        for (int i = 0; i < data.Length; i ++)
+        {
+            data.components[i].theta += t;
+        }
+    }
+}
+
